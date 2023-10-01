55fe9d04b258ef15682396fa0a9ec4489ca95013
zkiyoung <2505252034@qq.com>
2023-10-01 22:36:15 +0800

    121.买卖股票的最佳时机

---
+++ src/test/java/Test121.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+/**
+ * @Description: 买卖股票的最佳时机
+ * @Auther: Zki Young
+ * @Date: 2023/10/1 22:26
+ * @company：CTTIC
+ */
+public class Test121 {
+    @Test
+    public void testMain() {
+        System.out.println(maxProfit(new int[]{7, 2, 4, 1, 9}));
+    }
+
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for (int price : prices) {
+            if (price < min) {
+                min = price;
+            } else {
+                max = Math.max(max, price - min);
+            }
+        }
+        return max;
+    }
+}
