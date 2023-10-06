2841132ca7e12511848ec9b0cf3d51cebece0b9f
zkiyoung <2505252034@qq.com>
2023-10-07 06:57:35 +0800

    231. 2 的幂

---
+++ src/test/java/easy/Test231.java
@@ -0,0 +1,29 @@
+package easy;
+
+import org.junit.Test;
+
+/**
+ * @Description: 231. 2 的幂
+ * @Auther: Zki Young
+ * @Date: 2023/10/7 06:41
+ * @company：CTTIC
+ */
+public class Test231 {
+    @Test
+    public void testMain(){
+        System.out.println(isPowerOfTwo(32));
+        System.out.println(isPowerOfTwo(32));
+    }
+
+    public boolean isPowerOfTwo(int n) {
+        if (n == 1) return true;
+        if (n == 0) return false;
+        while(n % 2 == 0){
+            n /= 2;
+            if (n == 1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
