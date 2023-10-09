77c3e102db410dd64b61d5ae265e1be41eb2c749
zkiyoung <2505252034@qq.com>
2023-10-09 19:54:11 +0800

    2578. 最小和分割

---
+++ src/test/java/easy/Test2578.java
@@ -0,0 +1,42 @@
+package easy;
+
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * @Description: 最小和分割
+ * @Auther: Zki Young
+ * @Date: 2023/10/9 19:33
+ * @company：CTTIC
+ */
+public class Test2578 {
+
+    @Test
+    public void testMain() {
+        System.out.println(splitNum(4325));
+        System.out.println(splitNum(687));
+    }
+
+    public int splitNum(int num) {
+        List<Integer> list = new ArrayList<>();
+        while (num > 0) {
+            list.add(num % 10);
+            num /= 10;
+        }
+        Collections.sort(list);
+        int i = list.size() / 2 * 2;
+        int ans = 0;
+        int ans02 = 0;
+        for (int j = 0; j < i; j += 2) {
+            int remove = list.remove(0);
+            ans = ans * 10 + remove;
+            remove = list.remove(0);
+            ans02 = ans02 * 10 + remove;
+        }
+        if (list.size() != 0) {
+            ans = ans * 10 + list.remove(0);
+        }
+        return ans + ans02;
+    }
+}
---
+++ src/test/java/el/TestElse.java
@@ -0,0 +1,15 @@
+package el;
+
+import org.junit.Test;
+
+/**
+ * @Description: TODO
+ * @Auther: Zki Young
+ * @Date: 2023/10/8 08:26
+ * @company：CTTIC
+ */
+public class TestElse {
+    @Test
+    public void testMain(){
+    }
+}
