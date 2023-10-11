659564b9ce32731eb76bab443d694114a83d1a59
zkiyoung <2505252034@qq.com>
2023-10-11 09:10:17 +0800

    234. 回文链表

--- src/test/java/leetcode/easy/Test234.java
+++ src/test/java/leetcode/easy/Test234.java
@@ -3,6 +3,8 @@ package leetcode.easy;
 import leetcode.domain.ListNode;
 import org.junit.Test;
 
+import java.util.*;
+
 /**
  * @Description: 234. 回文链表
  * @Auther: Zki Young
@@ -15,6 +17,7 @@ public class Test234 {
 
     }
 
+
     public boolean isPalindrome(ListNode head) {
         StringBuilder stringBuilder = new StringBuilder();
         while (head != null) {
@@ -22,5 +25,20 @@ public class Test234 {
             head = head.next;
         }
         return stringBuilder.toString().equals(stringBuilder.reverse().toString());
+    }
+
+    //双指针
+    public boolean isPalindrome02(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        for (int i = 0; i < list.size() / 2; i++) {
+            if (list.get(i) != list.get(list.size() - 1 - i)){
+                return false;
+            }
+        }
+        return true;
     }
 }
